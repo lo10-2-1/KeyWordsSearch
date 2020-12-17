35c72f6e7cc7e2cc1b283ec778f760b2d8f2c9bf
lo10-2-1 <lidrudakova@gmail.com>
2020-12-17 18:21:11 +0300

    DB module completed

--- db_converting.py
+++ db_converting.py
@@ -6,7 +6,7 @@ def create_index(client):
     '''Creates an index in Elasticsearch.
     '''
     client.indices.create(
-        index="DB",
+        index="db",
         body={
             "settings": {"number_of_shards": 1},
             "mappings": {
@@ -22,51 +22,51 @@ def create_index(client):
     )
 
 
-def count_docs(dataset):
-    '''Reads .csv file and returns number of rows as an integer.
+def count_docs(path):
+    '''Reads the file through csv.DictReader()
+    and returns number of rows as an integer.
     '''
-    return sum([1 for row in dataset])
+    with open(path, encoding='utf-8') as f:
+            reader = csv.DictReader(f)
+            return sum([1 for row in reader])
 
 
-def generate_actions(dataset):
-    '''Yields a single document in index for each row.
+def generate_actions(path):
+    '''Reads the file through csv.DictReader()
+    and yields a single document in index for each row.
     '''
-    for row in dataset:
-        document = {
-            "text": row["text"],
-            "created_date": row["created_date"],
-            "rubrics": row["rubrics"],
-        }
-        yield document
+    with open(path, encoding='utf-8') as f:
+            reader = csv.DictReader(f)
+        
+            for row in reader:
+                document = {
+                    "text": row["text"],
+                    "created_date": row["created_date"],
+                    "rubrics": row["rubrics"],
+                }
+                yield document
 
 
-def converting(path, client):
-    '''Reads the file through csv.DictReader() and for each row
-    yields a single document.
+def converting(path, client, helper):
+    '''Reads the file through csv.DictReader() and converts it to
+    the ElasticSearch database.
     '''
-    if dataset_path[-4:] != '.csv':
+    if path[-4:] != '.csv':
         return print('Incorrect path or type of file. Try again.')
 
     try:
-        with open(dataset_path, encoding='utf-8') as f:
-            reader = csv.DictReader(f)
-            number_of_docs = count_docs(reader)
-        except FileNotFoundError:
+        create_index(client)
+        number_of_docs = count_docs(path)
+
+        successes = 0
+        for ok, action in helper(
+            client=client, index='db', actions=generate_actions(path),
+        ):
+            successes += ok
+        print("Database created! Indexed %d/%d documents" % (successes, number_of_docs))
+
+    except FileNotFoundError:
         print('Incorrect path. Try again.')
-
-    number_of_docs = db.count_docs(path)
-    create_index(client)
-    
-    progress = tqdm.tqdm(unit='docs', total=number_of_docs)
-
-    successes = 0
-    for ok, action in streaming_bulk(
-        client=es, index='DB', actions=generate_actions(),
-    ):
-        progress.update(1)
-        successes += ok
-    print("Indexed %d/%d documents" % (successes, number_of_docs))
-    print(number_of_docs)
 
 
 if __name__ == '__main__':
--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import json
 from pprint import pprint
-from elasticsearch import helpers, Elasticsearch
+from elasticsearch import Elasticsearch
+from elasticsearch.helpers import streaming_bulk
 
 import db_converting as db
 
@@ -8,7 +9,7 @@ es = Elasticsearch([{'host': 'localhost', 'port': 9200}])
 CSV_PATH = input('Enter csv path: ')
 
 def main():
-    db = db.converting(CSV_PATH, es)
+    db.converting(CSV_PATH, es, streaming_bulk)
 
 if __name__ == '__main__':
     main()
